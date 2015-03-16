556c74b4ba19180182fb6a0c66ed9f06179a3424
higs4281 <higs4281@gmail.com>
2015-03-16 15:56:44 -0400

    prep setups

--- env_setup.sh
+++ env_setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 PROJECT_NAME="retirement"
-DJ_KEY="mbu2b9$ymrbt1h8%_hlxcp370siw2)v_c0s$)#g)4wgi9f9d4*"
-
-#echo "export DJ_KEY='mbu2b9$ymrbt1h8%_hlxcp370siw2)v_c0s$)#g)4wgi9f9d4*'" >> ~/.bash_profile
-#source ~/.bash_profile
--- setup.sh
+++ setup.sh
@@ -5,13 +5,11 @@ source globals.sh
 # echo "Creating directories"
 # mkdir data
 
-echo "Creating virtualenv"
-mkvirtualenv $PROJECT_NAME
+# echo "Creating virtualenv"
+# mkvirtualenv $PROJECT_NAME
 
-echo "Installing requirements"
-pip install -r requirements.txt
+# echo "Installing requirements"
+# pip install -r requirements.txt
 
 # echo "Downloading data"
-# python 
-
-# curl -L -o $FILENAME https://github.com/onyxfish/journalism/raw/master/examples/realdata/ks_1033_data.csv
+# python manage.py download_initial_data
