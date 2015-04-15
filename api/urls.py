6b31bd8264afa21a6d0b31a4dc2ca496e05cecd1
higs4281 <higs4281@gmail.com>
2015-04-15 18:38:53 -0400

    adjust api urls, add setup

--- api/urls.py
+++ api/urls.py
@@ -9,9 +9,9 @@ urlpatterns = patterns('',
 
     url(r'^claiming-social-security/$', 'api.views.main', name='main'),  
     url(r'^claiming-social-security/admin/', include(admin.site.urls)),  
-    url(r'^claiming-social-security/retire-api/estimator/$', 'api.views.estimator', name='estimator'),
-    url(r'^claiming-social-security/retire-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'api.views.estimator', name='estimator'),
-    url(r'^claiming-social-security/retire-api/get-retirement-age/(?P<birth_year>\d+)/$', 'api.views.get_full_retirement_age', name='get_full_retirement_age'),
+    url(r'^claiming-social-security/retirement-api/estimator/$', 'api.views.estimator', name='estimator'),
+    url(r'^claiming-social-security/retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'api.views.estimator', name='estimator'),
+    url(r'^claiming-social-security/retirement-api/get-retirement-age/(?P<birth_year>\d+)/$', 'api.views.get_full_retirement_age', name='get_full_retirement_age'),
     # url(r'^retire-api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 )
