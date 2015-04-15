ac6dd270a6dcdde237b562caddafd6a661572e37
higs4281 <higs4281@gmail.com>
2015-04-15 15:32:53 -0400

    base view

--- api/urls.py
+++ api/urls.py
@@ -7,10 +7,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'api.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),  
+    url(r'^claiming-social-security/$', 'api.views.main', name='main'),  
+    url(r'^claiming-social-security/admin/', include(admin.site.urls)),  
+    url(r'^claiming-social-security/retire-api/estimator/$', 'api.views.estimator', name='estimator'),
+    url(r'^claiming-social-security/retire-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'api.views.estimator', name='estimator'),
+    url(r'^claiming-social-security/retire-api/get-retirement-age/(?P<birth_year>\d+)/$', 'api.views.get_full_retirement_age', name='get_full_retirement_age'),
     # url(r'^retire-api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^retire-api/estimator/$', 'api.views.estimator', name='estimator'),
-    url(r'^retire-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'api.views.estimator', name='estimator'),
-    url(r'^retire-api/get-retirement-age/(?P<birth_year>\d+)/$', 'api.views.get_full_retirement_age', name='get_full_retirement_age'),
 
 )
