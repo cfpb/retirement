97ca2566407c86d824af99d75e9707334d35b5bb
higs4281 <higs4281@gmail.com>
2015-04-16 10:03:05 -0400

    park restframework

--- retirement_api/settings.py
+++ retirement_api/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    # 'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
