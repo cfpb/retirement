af848652005719da1ff34d59cf0bc621826c1ab6
higs4281 <higs4281@gmail.com>
2015-04-28 02:30:03 -0400

    fix url iports

--- retirement_api/urls.py
+++ retirement_api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^retirement-api/estimator/$', 'views.estimator', name='estimator'),
-    url(r'^retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'views.estimator', name='estimator'),
-    url(r'^retirement-api/get-retirement-age/(?P<birth_year>\d+)/$', 'views.get_full_retirement_age', name='get_full_retirement_age'),
+    url(r'^retirement-api/estimator/$', 'retirement_api.views.estimator', name='estimator'),
+    url(r'^retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'retirement_api.views.estimator', name='estimator'),
+    url(r'^retirement-api/get-retirement-age/(?P<birth_year>\d+)/$', 'retirement_api.views.get_full_retirement_age', name='get_full_retirement_age'),
 )
