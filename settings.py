adedc3f6f4727ea7e44f2707c898d7a48347cd06
higs4281 <higs4281@gmail.com>
2015-04-29 19:12:49 -0400

    local db specs

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'retirement_api',
+    'retirement_api',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,12 +57,24 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DJANGO_MYSQL_DBNAME'],
+        'USER': os.environ['DJANGO_MYSQL_USER'],
+        'PASSWORD': os.environ['DJANGO_MYSQL_PASSWORD'],
+        'HOST': os.environ['DJANGO_MYSQL_HOST'],
+        'PORT': os.environ.get('DJANGO_MYSQL_PORT', ''),
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
