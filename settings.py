652198522290b4ddc9527f1a7d76713497335159
higs4281 <higs4281@gmail.com>
2015-04-30 16:15:04 -0400

    drop unneeded mysql settings

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 TEMPLATE_DIRS = (
-    '%s/retirement_api/templates' % BASE_DIR,
+    '%s/templates' % BASE_DIR,
     )
 
 # Application definition
@@ -56,17 +56,22 @@ WSGI_APPLICATION = 'wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ['DJANGO_MYSQL_DBNAME'],
+#         'USER': os.environ['DJANGO_MYSQL_USER'],
+#         'PASSWORD': os.environ['DJANGO_MYSQL_PASSWORD'],
+#         'HOST': os.environ['DJANGO_MYSQL_HOST'],
+#         'PORT': os.environ.get('DJANGO_MYSQL_PORT', ''),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['DJANGO_MYSQL_DBNAME'],
-        'USER': os.environ['DJANGO_MYSQL_USER'],
-        'PASSWORD': os.environ['DJANGO_MYSQL_PASSWORD'],
-        'HOST': os.environ['DJANGO_MYSQL_HOST'],
-        'PORT': os.environ.get('DJANGO_MYSQL_PORT', ''),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
