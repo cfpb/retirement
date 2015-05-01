04acafe156cff7024bdeab963e42fe1de1c1655e
higs4281 <higs4281@gmail.com>
2015-04-30 23:28:00 -0400

    stop model test from creating objects

--- retirement_api/tests/test_models.py
+++ retirement_api/tests/test_models.py
@@ -1,21 +1,25 @@
 import os
 import sys
-import unittest
 from retirement_api.models import AgeChoice, Question
+
+from django.test import TestCase
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-class ViewModels(unittest.TestCase):
+class ViewModels(TestCase):
+
     testcase = AgeChoice.objects.get(age=67)
-    testquestion = Question(title='test q')
+
+    def setUp(self):
+        testquestion = Question(title='test q')
+        testquestion.save()
 
     def test_get_subhed(self):
         self.assertTrue("You've chosen age 67" in self.testcase.get_subhed())
 
     def test_question_slug(self):
-        self.testquestion.save()
-        self.assertTrue(self.testquestion.slug == "test_q")
-        self.testquestion.delete()
+        tq = Question.objects.get(title='test q')
+        self.assertTrue(tq.slug == "test_q")
 
