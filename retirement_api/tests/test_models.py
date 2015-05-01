28f256617e75310987d666e792991a0f0cc868b4
higs4281 <higs4281@gmail.com>
2015-04-30 22:40:49 -0400

    add model tests

---
+++ retirement_api/tests/test_models.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import unittest
+from retirement_api.models import AgeChoice, Question
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(BASE_DIR)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+class ViewModels(unittest.TestCase):
+    testcase = AgeChoice.objects.get(age=67)
+    testquestion = Question(title='test q')
+
+    def test_get_subhed(self):
+        self.assertTrue("You've chosen age 67" in self.testcase.get_subhed())
+
+    def test_question_slug(self):
+        self.testquestion.save()
+        self.assertTrue(self.testquestion.slug == "test_q")
+
