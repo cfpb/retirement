cbd78e2ca134729e99bac7d05e0117a75c828d12
higs4281 <higs4281@gmail.com>
2015-05-08 01:53:48 -0400

    add last translation bits, helper functions to models

--- retirement_api/models.py
+++ retirement_api/models.py
@@ -15,6 +15,10 @@ class Step(models.Model):
     def __unicode__(self):
         return self.title
 
+    def translist(self):
+        """returns list of fields that should be translated"""
+        return ['title', 'instructions', 'note']
+
 class AgeChoice(models.Model):
     age = models.IntegerField()
     aside = models.CharField(max_length=500)
@@ -22,6 +26,11 @@ class AgeChoice(models.Model):
     def get_subhed(self):
         return "You've chosen age %s. %s Here are some steps \
                 to help you in the next few years." % (self.age, self.aside)
+    
+    def translist(self):
+        """returns list of fields that should be translated"""
+        return ['aside']
+
     class Meta:
         ordering = ['age']
 
@@ -36,6 +45,10 @@ class Page(models.Model):
     step2 = models.ForeignKey(Step, related_name='step2', blank=True, null=True)
     step3 = models.ForeignKey(Step, related_name='step3', blank=True, null=True)
     final_steps = models.TextField(blank=True)
+    
+    def translist(self):
+        """returns list of fields that should be translated"""
+        return ['title', 'h1', 'intro', 'h2', 'h3', 'h4', 'final_steps']
 
     def __unicode__(self):
         return self.title
@@ -43,9 +56,23 @@ class Page(models.Model):
 class Tooltip(models.Model):
     title = models.CharField(max_length=500)
     text = models.TextField(max_length=255, blank=True)
+    
+    def translist(self):
+        """returns list of fields that should be translated"""
+        return ['text']
 
     def __unicode__(self):
         return self.title
+
+POHEADER = [
+    'msgid ""\n',
+    'msgstr ""\n',
+    '"MIME-Version: 1.0\\n"\n',
+    '"Content-Type: text/plain; charset=UTF-8\\n"\n',
+    '"Content-Transfer-Encoding: 8bit\\n"\n',
+    '"Project-Id-Version: retirement\\n"\n',
+    '"Language: es\\n"\n\n'
+    ]
 
 class Question(models.Model):
     title = models.CharField(max_length=500)
@@ -73,3 +100,44 @@ class Question(models.Model):
         if not self.slug:
             self.slug = slugify(self.title).replace('-', '_')
         super(Question, self).save(*args, **kwargs)
+    
+    def translist(self):
+        """returns list of fields that should be translated"""
+        fieldlist = [
+            "question",
+            "answer_yes_a_subhed",
+            "answer_yes_a",
+            "answer_yes_b_subhed",
+            "answer_yes_b",
+            "answer_no_a_subhed",
+            "answer_no_a",
+            "answer_no_b_subhed",
+            "answer_no_b",
+            "answer_unsure_a_subhed",
+            "answer_unsure_a",
+            "answer_unsure_b_subhed",
+            "answer_unsure_b",
+            ]
+        return fieldlist
+
+    def dump_translation_text(self, output=False):
+        """
+        translation utility
+        
+        returns a list of phrases to be translated, 
+        or outputs a utf-8 .po file to /tmp/
+        """
+        fieldlist = self.translist()
+        outfile = "/tmp/%s.po" % self.slug
+        phrases = [self.__getattribute__(attr) for attr in fieldlist if self.__getattribute__(attr)]
+        if output == True:
+            with open(outfile, 'w') as f:
+                for line in POHEADER:
+                    f.write(line.encode('utf-8'))
+                for phrase in phrases:
+                    f.write('#: templates/claiming.html\n'.encode('utf-8'))
+                    f.write(unicode('msgid "%s"\n' % phrase).encode('utf-8'))
+                    f.write('msgstr ""\n'.encode('utf-8'))
+                    f.write("\n")
+        else:
+            return phrases
