0bd2d180452bbef552554ce7d05d1709ffb2e679
higs4281 <higs4281@gmail.com>
2015-06-29 11:40:07 -0400

    restore timeout fix for the standalone app

--- retirement_api/utils/ss_calculator.py
+++ retirement_api/utils/ss_calculator.py
@@ -195,7 +195,7 @@ params = {
 }
 
 
-def get_retire_data(params):
+def get_retire_data(params, timeout=True):
     starter = datetime.datetime.now()
     collector = {}
     benefits = {}
@@ -242,12 +242,14 @@ def get_retire_data(params):
             results['note'] = "An invalid date was entered."
             results['error'] = past_fra
             return json.dumps(results)
-    signal.signal(signal.SIGALRM, handler)
-    signal.alarm(timeout_seconds)
+    if timeout:
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(timeout_seconds)
     try:
         req = requests.post(result_url, data=params)
     except requests.ConnectionError:
-        signal.alarm(0)
+        if timeout:
+            signal.alarm(0)
         results['error'] = "Social Security's website is not responding.\
                             Status code: %s (%s)" % (req.status_code,
                                                      req.reason)
@@ -259,7 +261,10 @@ def get_retire_data(params):
         results['note'] = down_note
         return json.dumps(results)
     else:
-        signal.alarm(0)
+        if timeout:
+            signal.alarm(0)
+        else:
+            pass
     if not req.ok:
         results['error'] = "Social Security's website is not responding.\
                             Status code: %s (%s)" % (req.status_code,
