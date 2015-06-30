05979ca4d740da45ce750e4a606c6da3b9a29d78
higs4281 <higs4281@gmail.com>
2015-06-30 06:21:09 -0400

    move up fixture import

--- retirement_api/tests/test_models.py
+++ retirement_api/tests/test_models.py
@@ -13,12 +13,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 class ViewModels(TestCase):
 
+    fixtures = ['retiredata.json']
     # testcase = AgeChoice.objects.get(age=62)
     testquestion = Question(title='test')
     teststep = Step.objects.all()[0]
     testpage = Page.objects.all()[0]
     testtip = Tooltip.objects.all()[0]
-    # fixtures = ['retiredata.json']
 
     # def test_errortext(self):
     #     test_et = ErrorText.objects.get(slug='too_old')
