74abe0a5dbd14253eafaff7c168f342d0e4e3890
higs4281 <higs4281@gmail.com>
2015-06-29 20:54:28 -0400

    no syncdb

--- retirement_api/tests/test_models.py
+++ retirement_api/tests/test_models.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from retirement_api.models import AgeChoice, Question, Step, Page, Tooltip
+from retirement_api.models import AgeChoice, Question, Step, Page
+from retirement_api.models import Tooltip, ErrorText
 import mock
 
 from django.test import TestCase
@@ -9,6 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
+
 class ViewModels(TestCase):
 
     testcase = AgeChoice.objects.get(age=62)
@@ -16,6 +18,10 @@ class ViewModels(TestCase):
     teststep = Step.objects.all()[0]
     testpage = Page.objects.all()[0]
     testtip = Tooltip.objects.all()[0]
+
+    def test_errortext(self):
+        test_et = ErrorText.objects.get(slug='too_old')
+        self.assertTrue("sorry" in test_et.note)
 
     def test_get_subhed(self):
         tc = AgeChoice.objects.get(age=62)
@@ -30,7 +36,10 @@ class ViewModels(TestCase):
     def test_question_translist(self):
         tlist = self.testquestion.translist()
         self.assertTrue(type(tlist) == list)
-        for term in ['question', 'answer_yes_a', 'answer_no_b', 'answer_unsure_a_subhed']:
+        for term in ['question',
+                     'answer_yes_a',
+                     'answer_no_b',
+                     'answer_unsure_a_subhed']:
             self.assertTrue(term in tlist)
 
     def test_quesiton_dump(self):
@@ -46,7 +55,7 @@ class ViewModels(TestCase):
             mock_open.return_value = mock.MagicMock(spec=file)
             self.testquestion.dump_translation_text(output=True)
             file_handle = mock_open.return_value.__enter__.return_value
-            file_handle.write.assert_call_count==5
+            file_handle.write.assert_call_count == 5
 
     def test_agechoice_translist(self):
         tlist = self.testcase.translist()
