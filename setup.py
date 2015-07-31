5cc51d188cbf867f5ef94c051d567c90e963eaa2
william higgins <higs4281@users.noreply.github.com>
2015-07-31 11:36:53 -0400

    update version to 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_file(filename):
 
 setup(
     name='retirement',
-    version='0.1.7',
+    version='0.2.0',
     author='CFPB',
     author_email='tech@cfpb.gov',
     maintainer='cfpb',
