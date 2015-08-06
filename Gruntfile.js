acf92851a327f0e243f3c070ad5c19a88fcc9420
Chuck Werner <mistergone@gmail.com>
2015-08-06 15:54:23 -0400

    Adds gruntify-eslint and a corresponding 'grunt lint' task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,10 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('gruntify-eslint');
 
   grunt.registerTask('build', ['concat']);
+  grunt.registerTask('lint', ['eslint']);
   grunt.registerTask('default', ['concat']);
 
   grunt.initConfig({
@@ -15,6 +17,13 @@ module.exports = function(grunt) {
         src: ['src/claiming-functions.js', 'src/claiming-graph.js'],
         dest: 'retirement_api/static/retirement/js/claiming-social-security.js',
       },
+    },
+    eslint: {                   
+      options: {
+        useEslintrc: false,
+        configFile: "conf/eslint.yaml"
+      },
+      src: ["src/*.js"]
     },
     watch: {
       js: {
