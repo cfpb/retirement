be73e4a748292e9266fb9a48962e436a7c57b465
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2015-08-25 21:20:53 -0400

    Add build_ext and bdist_egg overrides

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os
 from setuptools import setup, find_packages
 from subprocess import call
 from setuptools import Command
+from distutils.command.build_ext import build_ext as _build_ext
+from setuptools.command.bdist_egg import bdist_egg as _bdist_egg
 
 
 def read_file(filename):
