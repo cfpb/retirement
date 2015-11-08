02b3509fa7fd78d7f75dd545048b3a73a816e466
higs4281 <higs4281@gmail.com>
2015-11-07 19:37:31 -0500

    remove unneeded urls; redirects better handled in apache

--- retirement_api/urls.py
+++ retirement_api/urls.py
@@ -6,15 +6,12 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     # url(r'^retirement-api/admin/', include(admin.site.urls)),
-    url(r'^$', 'retirement_api.views.claiming', name='landing_page'),
-    url(r'^es/$', 'retirement_api.views.claiming', name='landing_page_es'),
     url(r'^before-you-claim/$', 'retirement_api.views.claiming', name='claiming'),
     url(r'^before-you-claim/es/$', 'retirement_api.views.claiming', {'es': True}, name='claiming_es'),
     url(r'^retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'retirement_api.views.estimator', name='estimator'),
     url(r'^retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/es/$', 'retirement_api.views.estimator', {'language': 'es'}, name='estimator_es'),
     url(r'^retirement/retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'retirement_api.views.estimator', name='estimator'),
     url(r'^retirement/retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/es/$', 'retirement_api.views.estimator', {'language': 'es'}, name='estimator_es'),
-    url(r'^retirement-api/get-retirement-age/(?P<birth_year>\d+)/$', 'retirement_api.views.get_full_retirement_age', name='get_full_retirement_age'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
