5c4ef04780ba813cca72aa0274282e9b2a3b479f
Nicholas Johnson <nicholas.johnson@cfpb.gov>
2016-04-06 13:46:47 -0400

    Disable placeholder about page

--- retirement_api/urls.py
+++ retirement_api/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     # url(r'^retirement-api/admin/', include(admin.site.urls)),
-    url(r'^before-you-claim/about/$', 'retirement_api.views.about', name='about'),
-    url(r'^before-you-claim/about/es/$', 'retirement_api.views.about', {'language': 'es'}, name='about_es'),
+    # url(r'^before-you-claim/about/$', 'retirement_api.views.about', name='about'),
+    # url(r'^before-you-claim/about/es/$', 'retirement_api.views.about', {'language': 'es'}, name='about_es'),
     url(r'^before-you-claim/$', 'retirement_api.views.claiming', name='claiming'),
     url(r'^before-you-claim/es/$', 'retirement_api.views.claiming', {'es': True}, name='claiming_es'),
     url(r'^retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$', 'retirement_api.views.estimator', name='estimator'),
