cf970e2e76d382785206ec181b23bd02758b0935
higs4281 <higs4281@gmail.com>
2016-04-29 10:20:38 -0400

    remove unneeded imports

--- retirement_api/tests/test_commands.py
+++ retirement_api/tests/test_commands.py
@@ -1,27 +1,12 @@
-import sys
-import os
-import datetime
-import json
 import mock
+import unittest
 
 from django.core.management import call_command
-from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.test import TestCase
-import unittest
-from django.http import HttpRequest
-from django.conf import settings
 
 from retirement_api.management.commands import check_ssa_values
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-sys.path.append(BASE_DIR)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
 
 class CommandTests(unittest.TestCase):
-
     @mock.patch('retirement_api.management.commands.check_ssa_values.ssa_check.run_tests')
     def test_check_ssa_values(self, mock_run_tests):
         mock_run_tests.return_value = 'OK'
