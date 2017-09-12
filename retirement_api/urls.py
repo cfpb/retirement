f7110dd1549e6f3a2a1c37b6e48c17e457e25297
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2017-09-12 10:17:40 -0700

    Restore urls.py

--- retirement_api/urls.py
+++ retirement_api/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib import admin
 
 try:
-    STANDALONE = False
+    STANDALONE = settings.STANDALONE
 except AttributeError:  # pragma: no cover
     STANDALONE = False
 
@@ -23,3 +23,22 @@ urlpatterns = [
         {'language': 'es'}, name='estimator_es'),
     ]
 
+if STANDALONE:
+    admin.autodiscover()
+    urlpatterns = [
+        url(r'^retirement/admin/', include(admin.site.urls)),
+        url(r'^retirement/before-you-claim/about/$',
+            'retirement_api.views.about', name='retirement_about'),
+        url(r'^retirement/before-you-claim/about/es/$',
+            'retirement_api.views.about',
+            {'language': 'es'}, name='retirement_about_es'),
+        url(r'^retirement/before-you-claim/$',
+            'retirement_api.views.claiming', name='claiming'),
+        url(r'^retirement/before-you-claim/es/$',
+            'retirement_api.views.claiming', {'es': True}, name='claiming_es'),
+        url(r'^retirement/retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/$',
+            'retirement_api.views.estimator', name='estimator'),
+        url(r'^retirement/retirement-api/estimator/(?P<dob>[^/]+)/(?P<income>\d+)/es/$',
+            'retirement_api.views.estimator',
+            {'language': 'es'}, name='estimator_es'),
+        ]
