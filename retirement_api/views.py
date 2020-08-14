43b73cf973f80f313b4c525261d22c66e1b33d9e
Alex Martin <alexm118@vt.edu>
2020-08-14 09:21:40 -0400

    Include the language key for the mega menu (#259)
    * Include the lnaguage key for the mega menu
    
    * Remove print
    
    Co-authored-by: Alex Martin <alexander.martin@cfpb.gov>

--- retirement_api/views.py
+++ retirement_api/views.py
@@ -56,6 +56,7 @@ def claiming(request, es=False):
         "base_template": base_template,
         "available_languages": ["en", "es"],
         "es": es,
+        "language": language,
         "about_view_name": "retirement_api:" + ("about_es" if es else "about"),
     }
 
